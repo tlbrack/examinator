730605136da20f6e2f7d5a1563bd0d4b1360714e
Travis Brack <travis@cubi.com>
2022-06-28 21:41:20 -0400

    generate College

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_010201) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_012355) do
+  create_table "colleges", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
