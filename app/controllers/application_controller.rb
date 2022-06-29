4a6da6c9ff33ade6707a0dd6ecd795ac06094d9e
Travis Brack <travis@cubi.com>
2022-06-29 18:31:04 -0400

    log requests and responses to the db as suggested in challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  # simple request logging to the db
+  before_action :log_request
+  after_action :log_response
+
+  protected
+
+  def log_request
+    @apilog = ApiLog.create(
+      ip: request.ip,
+      request_uri: request.url,
+      request_method: request.method,
+      request_body: request.body.read,
+      request_params: request.params.to_json
+    )
+  end
+
+  def log_response
+    return unless @apilog.present?
+
+    @apilog.update(
+      response_body: response.body,
+      response_status_code: response.status
+    )
+  end
 end
