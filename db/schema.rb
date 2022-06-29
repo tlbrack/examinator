70d75d2e72524a92725706d92f5ce6dfa4a0c5e0
Travis Brack <travis@cubi.com>
2022-06-28 21:48:40 -0400

    generate exam

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_012355) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_014314) do
   create_table "colleges", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "exams", force: :cascade do |t|
+    t.string "name"
+    t.integer "college_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["college_id"], name: "index_exams_on_college_id"
   end
 
   create_table "users", force: :cascade do |t|
